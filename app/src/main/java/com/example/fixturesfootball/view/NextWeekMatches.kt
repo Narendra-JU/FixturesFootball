3e18e7d57ee48a37d9ef9ff0191c15618eb26605
narendra.chatterjee <narendra.chatterjee10!>
2021-05-29 18:52:21 +0530

    Added Dates and week formatting

--- app/src/main/java/com/example/fixturesfootball/view/NextWeekMatches.kt
+++ app/src/main/java/com/example/fixturesfootball/view/NextWeekMatches.kt
@@ -1,15 +1,18 @@
 package com.example.fixturesfootball.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fixturesfootball.R
 import com.example.fixturesfootball.model.countries.Countries
 import kotlinx.android.synthetic.main.fragment_next_week_matches.*
 import kotlinx.android.synthetic.main.fragment_previous_week_matches.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NextWeekMatches : Fragment() {
 
@@ -48,12 +51,23 @@ class NextWeekMatches : Fragment() {
                 temp3.add(arrayList[i + 12])
             }
         }
+        val calendar: Calendar = Calendar.getInstance()
+        val today: Date = calendar.getTime()
+
+        calendar.add(Calendar.WEEK_OF_MONTH, 1)
+        val nextWeek: Date = calendar.getTime()
+        val dateFormat: DateFormat = SimpleDateFormat("dd-MMM-yyyy")
+
+
+        val nextweekString: String = dateFormat.format(nextWeek)
+
+
 
         for (i in 0..3) {
             for (j in 0..3) {
                 if (j != i) {
-                    fixture.add("Group C ${temp2.get(j)} vs ${temp2.get(i)}")
-                    fixture.add("Group D ${temp3.get(j)} vs ${temp3.get(i)}")
+                    fixture.add("$nextweekString Group C ${temp2.get(j)} vs ${temp2.get(i)} Yet to be Held")
+                    fixture.add("$nextweekString Group D ${temp3.get(j)} vs ${temp3.get(i)} Yet to be Held")
                 }
 
             }
@@ -62,7 +76,7 @@ class NextWeekMatches : Fragment() {
         var no=0
         for (i in fixture){
             no += 1
-            countriesArrayList.add(Countries.Result("$no",i,""))
+            countriesArrayList.add(Countries.Result("$no", i, ""))
         }
 
         rv_nextDayFixtures.layoutManager= LinearLayoutManager(context)
--- app/src/main/java/com/example/fixturesfootball/view/PreviousWeekMatches.kt
+++ app/src/main/java/com/example/fixturesfootball/view/PreviousWeekMatches.kt
@@ -1,17 +1,16 @@
 package com.example.fixturesfootball.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fixturesfootball.R
 import com.example.fixturesfootball.model.countries.Countries
 import kotlinx.android.synthetic.main.fragment_previous_week_matches.*
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class PreviousWeekMatches : Fragment() {
@@ -51,12 +50,34 @@ class PreviousWeekMatches : Fragment() {
                 temp3.add(arrayList[i + 12])
             }
         }
+        val currentDate = SimpleDateFormat("dd/MM/yyyy")
+        val todayDate = Date()
+        val thisDate = currentDate.format(todayDate)
 
         for (i in 0..3) {
             for (j in 0..3) {
                 if (j != i) {
-                    fixture.add("Group A ${temp0.get(j)} vs ${temp0.get(i)}")
-                    fixture.add("Group B ${temp1.get(j)} vs ${temp1.get(i)}")
+                    var rnds1 = (0..5).random()
+                    var rnds2 = (0..5).random()
+                    var text=""
+                    text = if (rnds1>rnds2){
+                        "Team 1 WIN"
+                    }else if (rnds1<rnds2){
+                        "Team 2 WIN"
+                    }else{
+                        "It's a DRAW"
+                    }
+                    fixture.add("$thisDate Group A ${temp0.get(j)} vs ${temp0.get(i)} Score: $rnds1-$rnds2 $text")
+                    rnds1 = (0..5).random()
+                    rnds2 = (0..5).random()
+                    text = if (rnds1>rnds2){
+                        "Team 1 WIN"
+                    }else if (rnds1<rnds2){
+                        "Team 2 WIN"
+                    }else{
+                        "It's a DRAW"
+                    }
+                    fixture.add("$thisDate Group B ${temp1.get(j)} vs ${temp1.get(i)} Score: $rnds1-$rnds2  $text")
                 }
 
             }
@@ -66,7 +87,7 @@ class PreviousWeekMatches : Fragment() {
         for (i in fixture){
 
             no += 1
-            countriesArrayList.add(Countries.Result("$no",i,""))
+            countriesArrayList.add(Countries.Result("$no", i, ""))
         }
         rv_previousDayMatches.layoutManager=LinearLayoutManager(context)
         rv_previousDayMatches.adapter=CountriesRecyclerAdapter(countriesArrayList)
